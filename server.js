e1089b33412d638e35c1ed25b30916fa7368b7a3
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-23 11:17:37 -0600

    server js work

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// pick another npm package to give unique ids to notes
 
-const PORT = process.env.PORT || 3001
+const notesData = require('./db/db.json');
+
+// const PORT = 3001;
 const app = express();
 
+// set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -14,10 +19,19 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './notes/index
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
+//ask tutor about slice
+app.get('/api/notes', (req, res) => {
+	res.json(notesData.slice(1))
+	});
 
-
-
-
+	// not finsished
+app.delete('/api/notes/:id', (req, res) => {
+	fs.readFile('.db/db.json')
+});
+// param with id of note to delete
+// need filter to run thru notes and find ones with given id property
+// remove specified id property
+// rewrite the notes to the db.json file
 
 
 
